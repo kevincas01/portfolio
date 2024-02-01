887476e30644dfdbb7656c9072dff52486d6f39d
Kevin Castro <kcapupp@gmail.com>
2024-02-01 00:39:51 -0500

    navbar menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import '../Styles/navbar.css'
 
-const Navbar = ({aboutRef,experienceRef,projectsRef}) => {
+const Navbar = ({aboutRef,experienceRef,projectsRef,contactRef}) => {
     const [scrolled, setScrolled] = React.useState(false);
+
+    const [isNavToggled,setIsNavToggled]=React.useState(false)
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -16,38 +18,72 @@ const Navbar = ({aboutRef,experienceRef,projectsRef}) => {
         window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+
+    
     return (
+      <>
+      
     <div className={scrolled ? 'navbar scrolled' : 'navbar'}>
         <div className="nav-name">
           
           <span style={{fontWeight:"bold"}}>Kevin C</span>
+
         </div>
-        <div className='hamburger'>=</div>
-        <div className="nav-links">
-          
+        <div className={`menu ${isNavToggled ? 'menu-toggled' : ''}`} onClick={()=>{
+          console.log(!isNavToggled)
+          setIsNavToggled(!isNavToggled);
+        }}>
+
+          <span className="line"></span>
+            <span className="line"></span>
+        </div>
+
+        <div className={`nav-links ${isNavToggled ? 'nav-toggled' : ''}`}>
+
           <span className="nav-item"
-          onClick={() => aboutRef.current.scrollIntoView({  behavior: 'smooth',
+          onClick={() => { setIsNavToggled(false)
+            aboutRef.current.scrollIntoView({  behavior: 'smooth',
           block: 'start',
-          inline: 'nearest', })}
+          inline: 'nearest', })}}
           >About</span>
           <span className="nav-item"
-          onClick={() => experienceRef.current.scrollIntoView({  behavior: 'smooth',
+          onClick={() => {setIsNavToggled(false)
+            experienceRef.current.scrollIntoView({  behavior: 'smooth',
           block: 'start',
-          inline: 'nearest',})}
+          inline: 'nearest',})}}
           >Experience</span>
           <span className="nav-item"
-          onClick={() => projectsRef.current.scrollIntoView({  behavior: 'smooth',
+          onClick={() => {setIsNavToggled(false)
+            projectsRef.current.scrollIntoView({  behavior: 'smooth',
           block: 'start',
-          inline: 'nearest',})}
+          inline: 'nearest',})}}
           >Projects</span>
           <span className="nav-item"
-          onClick={() => projectsRef.current.scrollIntoView({  behavior: 'smooth',
+          onClick={() => {setIsNavToggled(false)
+            contactRef.current.scrollIntoView({  behavior: 'smooth',
           block: 'start',
-          inline: 'nearest',})}
+          inline: 'nearest',})}}
           >Contact</span>
         </div>
 
+    </div>
+
+      <div className={`nav-bars ${isNavToggled ? 'toggled' : ''}`}>
+
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div key={index} 
+          className={`nav-bar ${isNavToggled ? 'toggled' : 'nontoggled'}`}
+          id={`${isNavToggled ? 'toggled-' : 'nontoggled-'}nav-bar-${index}`}
+          style={{ animationDelay: `${(index + 1)*0.05}s` }}
+
+          >
+
+          </div>
+        ))}
       </div>
+
+
+    </>
   )
 }
 
