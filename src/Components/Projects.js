7f168a6022c2cffa2802faa1e9d10b265417856b
Kevin Castro <kcapupp@princeton.edu>
2025-02-15 20:10:40 -0500

    Reword Project descriptions

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,15 +4,12 @@ const Projects = () => {
   const projectsList = [
     {
       title: "Vibify - Spotify social media app",
-      description: `A Fullstack web app that brings your Spotify experience to life with personalized data and social recommendations.
-View your most-listened-to artists, top tracks, recently played songs, and detailed audio information for each track. 
-The core feature of Vibify is the Recommendations Page, where you can explore new music, share your favorite artists, albums, songs, and playlists with others, and seamlessly add them to your personal playlists or save them directly to your Spotify profile.`,
+      description: `A full-stack web app that enhances your Spotify experience with personalized insights and social recommendations. Explore top artists, tracks, and recently played songs. The Recommendations Page lets you discover new music, share favorites, and seamlessly add them to your Spotify playlists.`,
       skills: ["Typescript", "Next", "Next Auth", "Spotify API"],
     },
     {
       title: "Vidload",
-      description: `A video streaming application that allows users to upload, edit, and view videos. To support these features, I built a Node.js API server that facilitates video and image uploads to an AWS S3 bucket, transfers media via AWS CloudFront for fast delivery, and stores user data securely in a PostgreSQL database. 
-      This architecture ensures smooth media handling and efficient data management for an optimized user experience.`,
+      description: `A video streaming application that allows users to upload, edit, and view videos. To support these features, I built a Node.js API server that facilitates video and image uploads to an AWS S3 bucket, transfers media via AWS CloudFront for fast delivery, and stores user data securely in a PostgreSQL database.`,
       skills: ["AWS S3 & Cloudfront", "Typescript", "React", "Authentication"],
     },
     {
@@ -22,13 +19,13 @@ The core feature of Vibify is the Recommendations Page, where you can explore ne
     },
     {
       title: "Tigerfit",
-      description: `A Fullstack fitness web app designed specifically for Princeton University students to track their workouts, visualize progress, and promote healthy lifestyles on campus, attracting over 100 active users. The app included a suite of charting and graphing tools to provide in-depth analytics and visual insights into user data, such as weight and lifted weight. The app was published on TigerApps, a Princeton University platform showcasing innovative, student-built applications that enhance campus life.`,
+      description: `A full-stack fitness app for Princeton students to track workouts and visualize progress, gaining 100+ active users. Features interactive charts and analytics on weight and lifted weight. Published on TigerApps, Princeton’s student-built app hub.`,
       skills: ["Python", "Flask", "Javascript", "HTML", "CSS"],
     },
     {
       title: "Spaceship Trainer",
       description: `Developed and designed a 3D first person shooting game built with the Three.js 3D computer graphics framework.`,
-      skills: ["Three.js", "Javascript", "CSS"],
+      skills: ["Three.js", "HTML", "Javascript", "CSS"],
     },
   ];
   return (
